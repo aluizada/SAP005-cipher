90454985f44c63179fb1d6755c1b0f2230315b29
aluizada <luizanunes.b@gmail.com>
2020-11-07 18:05:45 -0300

    CifradeCesarfinal

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
-const cipher = {
-  // ...
-};
+const cipher = {encode, decode}
+ 
+ function encode (chave, frase) {
+  
+    let palavra = "";
+    let deslocamento;
+    let converteMensagem;
 
-export default cipher;
+    for (let index = 0; index < frase.length; index++) {
+    let numeroASC = frase.charCodeAt(index);
+    deslocamento = ((numeroASC - 65 + chave) % 26) + 65;
+    converteMensagem = String.fromCharCode(deslocamento);
+    palavra += converteMensagem;
+    console.log(palavra)
+  } 
+return palavra;
+}
+
+ function decode (chave, frase) {
+   let palavra = "";
+   let deslocamento;
+    let converteMensagem;
+
+    for (let index = 0; index < frase.length; index++) {
+    let numeroASC = frase.charCodeAt(index);
+    deslocamento = ((numeroASC - 65 - chave) % 26) + 65;
+    converteMensagem = String.fromCharCode(deslocamento);
+    palavra += converteMensagem;
+    console.log(palavra)
+  
+}
+return palavra 
+}
+
+
+export default cipher; 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
+function clickEncode(event) {
+    event.preventDefault()
+    let chave = parseInt(document.getElementById("offset").value)
+    let frase = document.getElementById("mensagem").value
+    frase = frase.toUpperCase()
+    let retorna = cipher.encode(chave, frase);
+    console.log (retorna)
+
+    document.getElementById("mensagem").value = retorna;
+}
+
+function clickDecode(event) {
+    event.preventDefault()
+    let chave = parseInt(document.getElementById("offset").value)
+    let frase = document.getElementById("mensagem").value
+    frase = frase.toUpperCase()
+    let retorna = cipher.decode(chave, frase);
+    console.log (retorna)
+
+    document.getElementById("mensagem").value = retorna;
+
+}
+document.getElementById("CIFRAR").addEventListener("click", clickEncode)
+document.getElementById("DECIFRAR").addEventListener("click", clickDecode)
+
+
+
 
 console.log(cipher);
